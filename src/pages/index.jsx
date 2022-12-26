b647d217c9cdc19cf9522a1c9112f0dd65ac97de
koyo code <87898051+koyo-code@users.noreply.github.com>
2022-12-26 22:11:30 +0900

    template編集

---
+++ src/pages/index.jsx
@@ -0,0 +1,7 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "src/styles/Home.module.css";
+
+export default function Home() {
+  return <div className={styles.container}></div>;
+}
